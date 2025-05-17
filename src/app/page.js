cada4336c058572155c3fee2e3de8fb1834c04eb
omondisteven <omondisteven@gmail.com>
2025-05-17 04:34:27 +0300

    Fixed home page build errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 // /src/app/page
 import PaymentForm from "@/components/PaymentForm";
+import { Suspense } from 'react';
  
 export default function Home() {
   return (
     <section className="bg-gray-100 max-w-400 h-screen flex justify-center items-center">
+      <Suspense fallback={<div>Loading...</div>}>
       <PaymentForm />
+      </Suspense>
     </section>
   );
 }
